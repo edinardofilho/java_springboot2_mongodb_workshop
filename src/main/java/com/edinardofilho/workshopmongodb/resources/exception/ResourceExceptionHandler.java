cba06e4e10e78ae2d65cb03bd622dda81c37e805
Edinardo Filho <edinardofreitas@gmail.com>
2020-08-22 15:54:22 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/edinardofilho/workshopmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.edinardofilho.workshopmongodb.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.edinardofilho.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/edinardofilho/workshopmongodb/services/UserService.java
+++ src/main/java/com/edinardofilho/workshopmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.edinardofilho.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edinardofilho.workshopmongodb.domain.User;
 import com.edinardofilho.workshopmongodb.repository.UserRepository;
+import com.edinardofilho.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repo; 
-	
+	private UserRepository repo;
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
